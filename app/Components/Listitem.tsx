2fca59dda26e77e168425cec05728a00eb6843c6
Abdullah Ismail <abdullahismail1.work@gmail.com>
2024-09-19 20:12:48 +0100

    fix deployment error by adding types to props

--- app/Components/Listitem.tsx
+++ app/Components/Listitem.tsx
@@ -1,10 +1,17 @@
 import Link from "next/link";
 import Arrow from "../icons/Arrow";
 
-export default function Listitem({name, linkTo}) {
+type props = {
+	name: string;
+	linkTo: string;
+};
+export default function Listitem({ name, linkTo }: props) {
 	return (
 		<li className=" hover:text-myblue flex items-center gap-3 relative after:content-[''] after:absolute after:bottom-0 after:left-0 after:top-8 after:w-full after:h-[1px] after:bg-black  xs:after:absolute xs:after:bottom-auto xs:after:left-auto xs:after:top-auto xs:after:w-0 xs:after:h-0 xs:after:bg-transparent ">
-			<Link href={linkTo} className="flex items-center gap-3 w-full justify-between ">
+			<Link
+				href={linkTo}
+				className="flex items-center gap-3 w-full justify-between "
+			>
 				<span>{name}</span>
 				<span className="xs:hidden">
 					<Arrow />
